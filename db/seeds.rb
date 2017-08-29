5d331f8bc50fda9f1289e773c1204df6f968db6c
jeferbc <jeffe.bernal@gmail.com>
2017-08-29 15:21:09 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |index|
+  Todo.create(name: "task #{index}", description: "description #{index}")
+end
